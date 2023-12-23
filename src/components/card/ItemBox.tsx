c41048c00a08ad28f720707ebcb4076be0460fa4
Joel Sung <joelsung123@gmail.com>
2023-12-23 12:10:16 +0800

    Add slider to controls

--- src/components/card/ItemBox.tsx
+++ src/components/card/ItemBox.tsx
@@ -1,16 +1,17 @@
-import Image from 'next/image'
-import styles from './ItemBox.module.scss'
+import Image from "next/image";
+import styles from "./ItemBox.module.scss";
 
 export default function ItemBox({ src, alt, onClick }) {
   return (
     <div className={styles.container}>
-      <div className={styles.overlayBackground}>
-      </div>
+      <div className={styles.overlayBackground}></div>
       <div className={styles.overlayDetails}>
         <div>{alt}</div>
-        <button onClick={onClick}>View Details</button>
+        <button onClick={onClick} className={styles.button}>
+          View Details
+        </button>
       </div>
       <Image src={src} alt={alt} className={styles.image} />
     </div>
-  )
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,35 @@
-import { faBriefcase, faLightbulb, faUser } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useState } from 'react';
-import JobSection from '../components/JobSection';
-import ProfileSection from '../components/ProfileSection';
-import ProjectSection from '../components/ProjectSection';
-import styles from './index.module.scss';
+import {
+  faBriefcase,
+  faLightbulb,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+import JobSection from "../components/JobSection";
+import ProfileSection from "../components/ProfileSection";
+import ProjectSection from "../components/ProjectSection";
+import styles from "./index.module.scss";
+
+interface ControlButtonProps {
+  onClick: () => void;
+  isActive: boolean;
+  styleId: string;
+  icon: any;
+}
+function ControlButton(props: ControlButtonProps) {
+  const { onClick, isActive, styleId, icon } = props;
+  return (
+    <button
+      onClick={onClick}
+      className={`
+          ${isActive ? styles.controlActive : styles.controlInactive} 
+          ${styleId}
+        `}
+    >
+      <FontAwesomeIcon icon={icon} className={styles.controlIcon} />
+    </button>
+  );
+}
 
 export default function HomePage({}) {
   const [profileActive, setProfileActive] = useState(true);
@@ -16,32 +41,36 @@ export default function HomePage({}) {
     setJobsActive(false);
     setProjectsActive(false);
     setActive(true);
-  }
+  };
 
   return (
     <div className={styles.main}>
       <div className={styles.sections}>
-        {profileActive && 
-          <ProfileSection className={styles.section} />
-        }
-        {jobsActive && 
-          <JobSection className={styles.section} />
-        }
-        {projectsActive &&
-          <ProjectSection className={styles.section} />
-        }
+        {profileActive && <ProfileSection className={styles.section} />}
+        {jobsActive && <JobSection className={styles.section} />}
+        {projectsActive && <ProjectSection className={styles.section} />}
       </div>
       <div className={styles.controls}>
-        <FontAwesomeIcon icon={faUser} 
+        <ControlButton
           onClick={() => changeActive(setProfileActive)}
-          className={profileActive ? styles.controlActive : styles.controlInactive} />
-        <FontAwesomeIcon icon={faBriefcase} 
+          isActive={profileActive}
+          styleId={styles.controlProfile}
+          icon={faUser}
+        />
+        <ControlButton
           onClick={() => changeActive(setJobsActive)}
-          className={jobsActive ? styles.controlActive : styles.controlInactive} />
-        <FontAwesomeIcon icon={faLightbulb} 
+          isActive={jobsActive}
+          styleId={styles.controlJobs}
+          icon={faBriefcase}
+        />
+        <ControlButton
           onClick={() => changeActive(setProjectsActive)}
-          className={projectsActive ? styles.controlActive : styles.controlInactive} />
+          isActive={projectsActive}
+          styleId={styles.controlProjects}
+          icon={faLightbulb}
+        />
+        <span className={styles.slider}></span>
       </div>
     </div>
-  )
+  );
 }
